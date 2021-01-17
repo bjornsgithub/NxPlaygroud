9182f6966fd0f0445f8ab36ae4bb5dcb1d7e0c86
bjornsgithub <bjornsmail@icloud.com>
2021-01-17 12:20:17 +0100

    feat: :art: Add switch style option  Add angular/cdk as dependency lib  Add pack and publish commands

--- libs/components/src/lib/components.component.ts
+++ libs/components/src/lib/components.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, Input, OnInit, Renderer2, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'nxplayground-components',
@@ -6,5 +7,36 @@ import { Component, ViewEncapsulation } from '@angular/core';
   template: '',
   encapsulation: ViewEncapsulation.None
 })
-export class ComponentsComponent {
+export class ComponentsComponent implements OnInit {
+
+  mainStyleContent: string = 'div {background-color: blue }';
+  alternativeStyleContent: string = 'div {background-color: red }';
+
+  @Input()
+  mainStyle: boolean = true;
+
+  public get styleContent() {
+    return this.mainStyle ? this.mainStyleContent : this.alternativeStyleContent;
+  }
+
+  constructor(private renderer: Renderer2, @Inject(DOCUMENT) private document: any) {}
+
+  ngOnInit() {
+
+    const styles = this.document.createElement('STYLE') as HTMLStyleElement;
+    styles.id = 'dynamic-theme-css';
+    styles.innerHTML = this.styleContent;
+    this.renderer.appendChild(this.document.head, styles);
+
+    const stylesTest = this.document.getElementById('dynamic-theme-css');
+  }
+
+  ngOnChanges() {
+
+    const styles = this.document.getElementById('dynamic-theme-css');
+
+    if (!styles) return;
+
+    styles.innerHTML = this.styleContent;
+  }
 }
